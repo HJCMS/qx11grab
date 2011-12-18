ca5cf97acd024b0e653bad6ccd92bdf37eb6816d
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2011-12-18 17:41:00 +0100

    So 18. Dez 20:56:48 CET 2011

---
+++ src/bookmark/bookmarkselecter.cpp
@@ -0,0 +1,72 @@
+/**
+* This file is part of the qx11grab project
+*
+* Copyright (C) Juergen Heinemann (Undefined) http://qx11grab.hjcms.de, (C) 2007-2012
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public License
+* along with this library; see the file COPYING.LIB.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+**/
+
+#include "bookmarkselecter.h"
+#include "bookmark.h"
+
+/* QtCore */
+#include <QtCore/QDebug>
+
+/* QtGui */
+#include <QtGui/QIcon>
+
+/* QtDBus */
+#include <QtDBus/QDBusConnection>
+
+BookmarkSelecter::BookmarkSelecter ( QWidget * parent )
+    : QComboBox ( parent )
+    , dbusPath ( "/BookmarkSelect" )
+{
+  setObjectName ( QLatin1String ( "BookmarkSelecter" ) );
+  QDBusConnection p_dbus = QDBusConnection::sessionBus();
+  p_dbus.registerObject ( dbusPath, this, ( QDBusConnection::ExportScriptableContents ) );
+  reload();
+  connect ( this, SIGNAL ( currentIndexChanged ( int ) ),
+            this, SLOT ( textChanged ( int ) ) );
+}
+
+void BookmarkSelecter::textChanged ( int index )
+{
+  if ( itemData ( index, Qt::UserRole ).toBool() )
+    emit openBookmark ( itemData ( index, Qt::DisplayRole ).toString() );
+}
+
+void BookmarkSelecter::reload()
+{
+  clear();
+  addItem ( QIcon::fromTheme ( "bookmark-toolbar" ), trUtf8 ( "Bookmark" ), false );
+  setEnabled ( false );
+
+  Bookmark xml;
+  if ( xml.open() )
+  {
+    foreach ( QString item, xml.entries() )
+    {
+      addItem ( QIcon::fromTheme ( "bookmark-toolbar" ), item, true );
+    }
+    setEnabled ( true );
+  }
+}
+
+BookmarkSelecter::~BookmarkSelecter()
+{
+  QDBusConnection::sessionBus().unregisterObject ( dbusPath, QDBusConnection::UnregisterNode );
+}
---
+++ src/bookmark/bookmarkselecter.h
@@ -0,0 +1,56 @@
+/**
+* This file is part of the qx11grab project
+*
+* Copyright (C) Juergen Heinemann (Undefined) http://qx11grab.hjcms.de, (C) 2007-2012
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Library General Public
+* License as published by the Free Software Foundation; either
+* version 2 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Library General Public License for more details.
+*
+* You should have received a copy of the GNU Library General Public License
+* along with this library; see the file COPYING.LIB.  If not, write to
+* the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+* Boston, MA 02110-1301, USA.
+**/
+
+#ifndef BOOKMARKSELECTER_H
+#define BOOKMARKSELECTER_H
+
+/* QtCore */
+#include <QtCore/QObject>
+#include <QtCore/QString>
+
+/* QtGui */
+#include <QtGui/QComboBox>
+#include <QtGui/QWidget>
+
+class BookmarkSelecter : public QComboBox
+{
+    Q_OBJECT
+    Q_CLASSINFO ( "Author", "Jürgen Heinemann (Undefined)" )
+    Q_CLASSINFO ( "URL", "http://qx11grab.hjcms.de" )
+
+  private:
+    const QString dbusPath;
+
+  private Q_SLOTS:
+    void textChanged ( int );
+
+  public Q_SLOTS:
+    Q_SCRIPTABLE void reload();
+
+  Q_SIGNALS:
+    void openBookmark ( const QString & );
+
+  public:
+    BookmarkSelecter ( QWidget * parent = 0 );
+    virtual ~BookmarkSelecter();
+};
+
+#endif
