3fc315862758ce668d86c62236b74be44cd30115
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2012-02-26 07:15:00 +0100

    So 26. Feb 09:28:44 CET 2012

--- src/application.cpp
+++ src/application.cpp
@@ -70,7 +70,7 @@ Application::Application ( int &argc, char **argv )
 
 #ifdef HAVE_OPENGL
 
-  if ( engine.compare ( "opengl" ) )
+  if ( engine.compare ( "opengl" ) == 0 )
   {
     if ( ! isEffectEnabled ( Qt::UI_AnimateToolBox ) )
       setEffectEnabled ( Qt::UI_AnimateToolBox, true );
