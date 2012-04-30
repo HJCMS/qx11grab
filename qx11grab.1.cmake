22247233e638a63b5a5f0a515aded89c176d077f
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2012-04-30 05:06:00 +0200

    Mo 30. Apr 09:10:19 CEST 2012

--- qx11grab.1.cmake
+++ qx11grab.1.cmake
@@ -6,18 +6,43 @@ QX11Grab \- a X11 Desktop Recording Software
 .B qx11grab
 .SH DESCRIPTION
 a systray application that uses FFmpeg for recording X11 desktops.
-qx11grab grab window dimensions from desktop and prepare the command line for ffmpeg with additional configuration options.
+qx11grab grab window dimensions from desktop and prepare the command line for FFmpeg with additional configuration options.
 
-qx11grab sends the generated parameter list to a new FFmpeg process.
+qx11grab sends the generated parameter list to a new \fIffmpeg\fR\|(1) process.
 
-Do not forget to test if Your FFmpeg installtion "x11grab" supported.
-
-.SH EXAMPLE
+.IP "\fBFeatures\fR"
 .nf
-ffmpeg -f x11grab -s svga -r 24 -b 6500 -bt 256k -sameq -i :0.0+1,33 /tmp/out.avi
+\[bu] Support for tapping Dimensions on Single/Multi and Xinerama Desktops,
+\[bu] Meta data input Formula,
+\[bu] Audio/Video Codec Table Editors with Context Menus and predefined Arguments,
+\[bu] Custom command Table editor for experts,
+\[bu] Command line Preview for better understanding FFmpeg option sets,
+\[bu] Configurable Audio capture Interface for Alsa/Pulse and OSS Devices,
+\[bu] Bookmark support for better switch Configuration to other codec sets,
+\[bu] Presets Editor for ffpresets,
+\[bu] Command line Shell script export.
 .fi
-.SH AUTHOR
-Juergen Heinemann (Undefined) <qx11grab(AT)hjcms(DOT)de>.
+.PP
+QX11Grab also implied a plugin interface for FFmpeg codec filters.
+.IP "\fBFilters\fR"
+.nf
+\[bu] Drawing text filter dialog,
+\[bu] Un/sharpen filter dialog,
+\[bu] Display Aspect Ratio (DAR) dialog,
+\[bu] Watermark filter dialog,
+\[bu] Fade (In/Out) filter dialog.
+.fi
+.PP
+
+.SH NOTE
+Do not forget to test if Your FFmpeg installtion "x11grab" supported.
+.nf
+ffmpeg -f x11grab -i :0.0+1,33 -s svga -r 24 -b:v 6500k -bt 256k -sameq -y /tmp/out.avi
+.fi
 .SH HOMEPAGE
 http://qx11grab.hjcms.de
+.SH AUTHOR
+Juergen Heinemann (Undefined) <nospam(AT)hjcms(DOT)de>.
+.SH SEE ALSO
+\&\fIffmpeg\fR\|(1) and the FFmpeg HTML documentation http://www.ffmpeg.org/documentation.html
 .PP
--- qx11grab.de.1.cmake
+++ qx11grab.de.1.cmake
@@ -1,25 +1,51 @@
 .IX Title "QX11GRAB 1"
 .TH QX11GRAB 1 "2011-06-30" "@QX11GRAB_VERSION_MAJOR@" "qx11grab"
-.SH NAME
-QX11Grab \- Ist eine X11 Desktop Aufnahme Software
-.SH SYNOPSIS
+.SH Name
+QX11Grab \- Ein hoch flexibles Digitales Bildschirmaufzeichner Programm für die X11 Desktop Oberfläche.
+.SH Programm Name
 .B qx11grab
-.SH DESCRIPTION
+.SH Beschreibung
 Ist ein Systemleisten Programm das mit FFmpeg den X11 Desktop auf nimmt.
 
-QX11Grab greift die Fenster Dimension ab und erstellt aus diesen
-Informationen und Zusaetzlichen Konfigurations- Optionen eine Kommandozeile fuer FFmpeg.
+QX11Grab greift die Fenster Dimension ab und erstellt aus diesen Informationen und Zusätzlichen Konfigurations- Optionen eine Kommandozeile für \fIffmpeg\fR\|(1).
 
-Die so generierten Parameter werden an einen neuen FFmpeg Prozess uebergeben.
+Die so generierten Parameter werden an einen neuen FFmpeg Prozess übergeben.
 
-Bitte stellen Sie sicher das Ihre FFmpeg Installation die Funktion "x11grab" Unterstuetzt.
-
-.SH EXAMPLE
+.IP "\fBAusstattung\fR"
 .nf
-ffmpeg -f x11grab -s svga -r 24 -b 6500 -bt 256k -sameq -i :0.0+1,33 /tmp/out.avi
+\[bu] Unterstützung zum abgreifen der Dimensionen auf Einzel/Multi und Xinerama Bildschirmen,
+\[bu] Formular zum einfügen der Kopfdaten,
+\[bu] Audio/Video Kodierer Eingabe Tabellen mit Kontext Menüs und vordefinierten Prädikaten,
+\[bu] Experten Eingabetabelle für Benutzerdefinierte Argumente,
+\[bu] Editierbare Kommandozeilen Vorschau zum besseren Verständnis der FFmpeg Optionen,
+\[bu] Konfigurierbare Audioaufnahme Schnittstelle für Alsa/Pulse und OSS,
+\[bu] Lesezeichen Unterstützung für ein besseres wechseln in den Kodierer Konfigurationen,
+\[bu] Prädikat bestimmungs- Editor für ffpresets,
+\[bu] Kommandozeilen export für Shell Scripte.
 .fi
-.SH AUTHOR
-Juergen Heinemann (Undefined) <qx11grab(AT)hjcms(DOT)de>.
-.SH HOMEPAGE
+.PP
+QX11Grab beinhaltet außerdem eine Plugin Schnittstelle für Filter Dialoge.
+.IP "\fBFilterliste\fR"
+.nf
+\[bu] Text Zeichnen Filter Dialog,
+\[bu] Un/Schärfen Filter Dialog,
+\[bu] Display Aspect Ratio (DAR) Dialog,
+\[bu] Wasserzeichen Filter Dialog,
+\[bu] Ein/Ausblenden Filter Dialog,
+\[bu] Webcam einbetten Filter.
+.fi
+.PP
+
+.SH Hinweis
+.I Bitte stellen Sie sicher das Ihre FFmpeg Installation die Funktion "x11grab" Unterstützt.
+.nf
+ffmpeg -f x11grab -i :0.0+1,33 -s svga -r 24 -b:v 6500k -bt 256k -sameq -y /tmp/out.avi
+.fi
+.SH Homepage
 http://qx11grab.hjcms.de
+.SH Autor
+.AU
+Jürgen Heinemann (Undefined) <nospam(AT)hjcms(DOT)de>.
+.SH Siehe auch
+\&\fIffmpeg\fR\|(1) und die FFmpeg HTML Dokumentation http://www.ffmpeg.org/documentation.html
 .PP
