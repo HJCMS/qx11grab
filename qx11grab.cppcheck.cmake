388c255eae905cc315a4ab928834cf1ae08a863d
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2013-07-13 05:19:00 +0200

    Sa 13. Jul 15:53:38 CEST 2013

---
+++ qx11grab.cppcheck.cmake
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<project version="1">
+    <includedir>
+      <dir name="src/"/>
+      <dir name="@CMAKE_CURRENT_BINARY_DIR@/src/"/>
+      <dir name="@QT_INCLUDE_DIR@/"/>
+      <dir name="@ALSA_INCLUDE_DIR@/"/>
+      <dir name="@PULSE_INCLUDEDIR@/"/>
+      <dir name="@FFMPEG_INCLUDEDIR@/"/>
+    </includedir>
+    <exclude>
+      <path name="@CMAKE_CURRENT_BINARY_DIR@/CMakeFiles/"/>
+      <path name="@CMAKE_CURRENT_BINARY_DIR@/CMakeTmp/"/>
+    </exclude>
+</project>
