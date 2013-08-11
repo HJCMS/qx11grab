eb5d016ef4541424d671dc109829e335796a5ddb
Jürgen Heinemann (Undefined) <nospam@hjcms.de>
2013-08-11 04:50:00 +0200

    So 11. Aug 06:21:09 CEST 2013

---
+++ qx11grab.app.cmake
@@ -0,0 +1,14 @@
+[Application]
+Name=qx11grab
+Version=1.0
+Description=A Application to record X11 desktops
+Type=X-ApplicationPackage
+Exec=qx11grab
+ExecType=application/x-executable
+Maintainer=Juergen Heinemann (Undefined) http://www.hjcms.de
+Authors=Juergen Heinemann (Undefined) http://www.hjcms.de
+Application-Version=@QX11GRAB_VERSION_MAJOR@.@QX11GRAB_VERSION_MINOR@.@QX11GRAB_VERSION_RELEASE@
+License=GPL3;Creative-Commons
+Icon=http://qx11grab.hjcms.de/qx11grab.svgz
+DownloadURL=ftp://ftp.hjcms.de/qx11grab/@QX11GRAB_VERSION_MAJOR@.@QX11GRAB_VERSION_MINOR@.@QX11GRAB_VERSION_RELEASE@/qx11grab-@QX11GRAB_VERSION_MAJOR@.@QX11GRAB_VERSION_MINOR@.@QX11GRAB_VERSION_RELEASE@.tar.xz
+UpdateInfo=http://qx11grab.hjcms.de/news.html
